15f6dd8331d2c8cc5cc538bc71c0004d58b71393
PeterCheq <Petercheq@gmail.com>
2025-08-13 04:07:38 +0200

    check if

--- app/(app)/search/page.js
+++ app/(app)/search/page.js
@@ -6,7 +6,7 @@ async function SearchPage({ searchParams }) {
   const { query } = await searchParams;
   console.log("title in question >>>>", query);
   const subreddits = await searchSubreddits(query);
-  console.log(">>><", subreddits);
+  console.log(">>><<", subreddits);
   return (
     <>
       <section className="bg-white border-b">
